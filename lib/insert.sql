1c776b592a60970639d93b91a8ee21ed49b36776
DispicableLee <lee.robert053@gmail.com>
2022-11-03 14:06:37 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Pooh", 3, "M", "yellow", "calm", "FALSE"),
+("Paddington", 4, "M", "brown", "calm", "TRUE"),
+("Paddington", 1, "M", "brown", "calm", "TRUE"),
+("Paddington", 5, "M", "brown", "calm", "TRUE"),
+("Paddington", 4, "M", "brown", "calm", "TRUE"),
+("Paddington", 3, "M", "brown", "calm", "TRUE"),
+("Paddington", 8, "M", "brown", "calm", "TRUE"),
+(NULL, 4, "F", "brown", "calm", "TRUE");
